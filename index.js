85ec44aa62dfea28a437fa502ca32ae3fe9ceb87
rileybrunn <ri570400@ucf.edu>
2024-02-02 15:26:44 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,7 @@
+const quoteStrings = [
+    "I want you to deal with your problems by becoming rich!",
+    "Whenever I'm about to do something, I think, 'Would an idiot do that?' and if they would, I do not do that thing.",
+    "Surprised, Eddie? If I woke up tomorrow with my head sewn to the carpet, I wouldn't be more surprised than I am right now."
+]
+
+console.log(quoteStrings[Math.floor(Math.random()*3)]);
